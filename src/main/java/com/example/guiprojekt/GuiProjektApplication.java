479c8646e67240b5b94d6fe2d4043ce15ca0f400
mejer96 <carlemilfalkmejer96@gmail.com>
2022-03-04 02:22:02 +0100

    fixed header. paragraphs og text til undersider.

--- src/main/java/com/example/guiprojekt/GuiProjektApplication.java
+++ src/main/java/com/example/guiprojekt/GuiProjektApplication.java
@@ -18,4 +18,24 @@ public class GuiProjektApplication {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/virksomheden")
+    public String virksomheden() {
+        return "virksomheden";
+    }
+
+    @GetMapping("/forbrugeren")
+    public String forbrugeren() {
+        return "forbrugeren";
+    }
+
+    @GetMapping("/om_os")
+    public String om_os() {
+        return "Om os";
+    }
+    @GetMapping("/om_madspild")
+    public String om_madspild() {
+        return "om madspild";
+    }
+
 }
